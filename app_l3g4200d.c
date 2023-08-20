08ba6e0d997422c025a67db088e622c9657ebcd4
tai.nguyen <tai.nguyen24816@gmail.com>
2023-08-20 15:41:34 +0700

    Update read xyz feature

--- app_l3g4200d.c
+++ app_l3g4200d.c
@@ -7,6 +7,12 @@
 /* ioctl command */
 #define L3G4200D_SWITCH_TO_NORMAL_MODE _IO(99, 1)
 #define L3G4200D_GET_INTERNAL_TEMP     _IOR(99, 2, void *)
+#define L3G4200D_GET_OUT_X_L           _IOR(99, 3, void *)
+#define L3G4200D_GET_OUT_X_H           _IOR(99, 4, void *)
+#define L3G4200D_GET_OUT_Y_H           _IOR(99, 5, void *)
+#define L3G4200D_GET_OUT_Y_L           _IOR(99, 6, void *)
+#define L3G4200D_GET_OUT_Z_H           _IOR(99, 7, void *)
+#define L3G4200D_GET_OUT_Z_L           _IOR(99, 8, void *)
 #define L3G4200D_DEVICE_FILE "/dev/l3g4200d_sensor"
 
 /**************************************************************************************** 
@@ -16,7 +22,7 @@ int main(void)
 {
     int fd = -1;
     int ret = -1;
-    uint8_t temperature_value = 0;
+    uint8_t temp = 0;
 
     fd = open(L3G4200D_DEVICE_FILE, O_RDWR);
     if(fd < 0)
@@ -32,14 +38,31 @@ int main(void)
         return -1;
     }
 
-    ret = ioctl(fd, L3G4200D_GET_INTERNAL_TEMP, &temperature_value);
-    if(ret != 0)
+    while(1)
     {
-        printf("ioctl error\n");
-        return -1;
-    }
+        printf("----------------------------------\n");
+        ioctl(fd, L3G4200D_GET_OUT_X_L, &temp);
+        printf("L3G4200D_GET_OUT_X_L = %d\n", temp);
 
-    printf("Raw temp is %d\n", temperature_value);
+        ioctl(fd, L3G4200D_GET_OUT_X_H, &temp);
+        printf("L3G4200D_GET_OUT_X_H = %d\n", temp);
+
+        ioctl(fd, L3G4200D_GET_OUT_Y_L, &temp);
+        printf("L3G4200D_GET_OUT_Y_L = %d\n", temp);
+
+        ioctl(fd, L3G4200D_GET_OUT_Y_H, &temp);
+        printf("L3G4200D_GET_OUT_Y_H = %d\n", temp);
+
+        ioctl(fd, L3G4200D_GET_OUT_Z_L, &temp);
+        printf("L3G4200D_GET_OUT_Z_L = %d\n", temp);
+
+        ioctl(fd, L3G4200D_GET_OUT_Z_H, &temp);
+        printf("L3G4200D_GET_OUT_Z_H = %d\n", temp);
+
+        printf("----------------------------------\n");
+
+        sleep(1);
+    }
 
     close(fd);
 
--- l3g42000_spi.c
+++ l3g42000_spi.c
@@ -39,6 +39,12 @@
 /* ioctl command */
 #define L3G4200D_SWITCH_TO_NORMAL_MODE _IO(99, 1)
 #define L3G4200D_GET_INTERNAL_TEMP     _IOR(99, 2, void *)
+#define L3G4200D_GET_OUT_X_L           _IOR(99, 3, void *)
+#define L3G4200D_GET_OUT_X_H           _IOR(99, 4, void *)
+#define L3G4200D_GET_OUT_Y_H           _IOR(99, 5, void *)
+#define L3G4200D_GET_OUT_Y_L           _IOR(99, 6, void *)
+#define L3G4200D_GET_OUT_Z_H           _IOR(99, 7, void *)
+#define L3G4200D_GET_OUT_Z_L           _IOR(99, 8, void *)
 
 
 /**************************************************************************************** 
@@ -122,14 +128,49 @@ static long l3g4200d_ioctl(struct file * flip, unsigned int cmd, unsigned long a
             break;
         case L3G4200D_GET_INTERNAL_TEMP:
             temp_value = l3g4200d_read_internal_temp();
-            pr_info("temp_value = %d\n", temp_value);
-            copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
             ret = 0;
             break;
-            
+        case L3G4200D_GET_OUT_X_L:
+            temp_value = l3g4200d_read_register(OUT_X_L);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
+        case L3G4200D_GET_OUT_X_H:
+            temp_value = l3g4200d_read_register(OUT_X_H);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
+        case L3G4200D_GET_OUT_Y_L:
+            temp_value = l3g4200d_read_register(OUT_Y_L);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
+        case L3G4200D_GET_OUT_Y_H:
+            temp_value = l3g4200d_read_register(OUT_Y_H);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
+        case L3G4200D_GET_OUT_Z_L:
+            temp_value = l3g4200d_read_register(OUT_Z_L);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
+        case L3G4200D_GET_OUT_Z_H:
+            temp_value = l3g4200d_read_register(OUT_Z_H);
+            ret = copy_to_user((u8 *)arg, &temp_value, 1);
+            ret = 0;
+            break;
+
         default:
             pr_info("Not support command\n");
             ret = -1;
+            break;
     }
 
     return ret;
@@ -206,8 +247,6 @@ u8 l3g4200d_read_internal_temp(void)
     u8 output_temp = 0;
 
     output_temp = l3g4200d_read_register(OUT_TEMP);
-
-    pr_info("output_temp = %d\n", output_temp);
 
     return output_temp;
 }
